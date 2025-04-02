56ef594f6ce8299c6ef4a425a07b4762547a75ad
NoahhhBHOP <noahhhbhop@gmail.com>
2025-04-02 12:31:58 -0500

    LESSON 3 DONE

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revString = string.split('').reverse().join('');
+    return revString;
 };
 
 // Do not edit below this line
