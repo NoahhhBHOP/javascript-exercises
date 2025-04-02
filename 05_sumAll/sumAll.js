65cd1c797ca24c246246c60752f14b0b244cb2d6
NoahhhBHOP <noahhhbhop@gmail.com>
2025-04-02 16:14:35 -0500

    goated

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sum = 0
+    if (num1 > num2) {
+        let holdNum = num1;
+        num1 = num2;
+        num2 = holdNum;
+    }
+    else if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    else if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return 'ERROR';
+    }
+    else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+    for (num1; num1 < num2; num1++)
+        sum += num1;
+        return sum + num2;
 };
 
 // Do not edit below this line
